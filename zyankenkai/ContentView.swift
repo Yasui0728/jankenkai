d78cb6721538a8f067c9efc65078ef51cd4168fa
Yasui0728 <ponta07285@gmail.com>
2025-11-27 08:25:15 +0900

    Initial Commit

---
+++ zyankenkai/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  zyankenkai
+//
+//  Created by 安井武士 on 2025/11/27.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ zyankenkai/zyankenkaiApp.swift
@@ -0,0 +1,17 @@
+//
+//  zyankenkaiApp.swift
+//  zyankenkai
+//
+//  Created by 安井武士 on 2025/11/27.
+//
+
+import SwiftUI
+
+@main
+struct zyankenkaiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
